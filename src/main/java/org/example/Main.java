ee50c9f206e68d57aa73b92db95181f43c86a58b
polk5072 <polk5072@naver.com>
2023-07-19 20:40:22 +0900

    New Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
